d08e4a6b8cfc71e75af1ba96da9b4d959ff8af7e
Keyboard Technology <Abdulrasheeds13@gmail.com>
2024-08-16 14:25:01 +0100

    created the send email function

--- emailing.py
+++ emailing.py
@@ -1,2 +1,32 @@
-def send_email():
-    print("Email was sent")
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+
+PASSWORD = "xuekhwymhdgcpdmd "
+SENDER = "abdulrasheeds13@gmail.com"
+RECEIVER = "abdulrasheeds13@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "Home Security Camera"
+    email_message.set_content("Hey, there is movement in your home garden perimeter")
+
+    with open (image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    # send the email
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email(image_path="images/36.png")
+
+
+
--- webcam.py
+++ webcam.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 from emailing import send_email
+import glob
+
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -12,8 +14,7 @@ count = 1
 while True:
     status = 0
     check, frame = video.read()
-    cv2.imwrite(f"images/{count}.png", frame)
-    count = count + 1
+
     grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(grey_frame, (21, 21), 0)
 
@@ -35,6 +36,11 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
 
     status_list.append(status)
